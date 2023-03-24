24581dd387bb2f6e7054ba7dd9f9afc49956143d
Basith P <basithpofficial@gmail.com>
2023-03-24 22:55:34 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/bns/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.bns.bloc_todos
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_staging.dart
@@ -0,0 +1,6 @@
+import 'package:bloc_todos/app/app.dart';
+import 'package:bloc_todos/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
